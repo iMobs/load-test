c445bd35ea27993b963f46fc6e7b115c4f01445c
Ian Mobley <ianmobley@gmail.com>
2017-12-27 15:52:45 -0800

    add a new column to the table

---
+++ migrations/20171227155056_display_name.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('users', (table) => {
+    table.string('display_name');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('users', (table) => {
+    table.dropColumn('display_name');
+  });
+};
